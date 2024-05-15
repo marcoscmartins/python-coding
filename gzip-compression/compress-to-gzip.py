35584480ee0e3e2f80e036d17bdfbf956ce7bf31
marcoscmartins <dev.marcosmartins@gmail.com>
2024-05-15 08:48:29 -0300

    fix: changing the name of the file

--- gzip-compression/compress-to-gzip.py
+++ gzip-compression/compress-to-gzip.py
@@ -3,12 +3,12 @@ import shutil
 
 def compress(input_file, output_file):
   with open(input_file, 'rb') as file_in:
-    with gzip.open(output_file, 'rb') as file_out:
+    with gzip.open(output_file, 'wb') as file_out:
       shutil.copyfileobj(file_in, file_out)
 
 # Exemplo de utilização
 
-file_input = 'example.txt'
-file_output = 'example.gz'
+file_input = './example.txt'
+file_output = './example.gz'
 
 compress(file_input, file_output)
