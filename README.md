94f5d382e648b1620ac3e2eaebfc9638622c1726
marcoscmartins <dev.marcosmartins@gmail.com>
2024-05-22 13:21:29 -0300

    feat: finalizing pdf to audio

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Python coding
 Repositório para salvar pequenos códigos em python
+
+### Programas existentes:
+
+- Cambio (requests)
+- Compressão de arquivos (gzip)
+- Conversor PDF to Audio (PyPDF2, pyttsx3)
--- pdf-to-audio/main.py
+++ pdf-to-audio/main.py
@@ -2,3 +2,26 @@ import PyPDF2
 import pyttsx3
 
 filename = input('Digite o nome do arquivo pdf incluindo a extensão').strip()
+
+try:
+  with open(filename, 'rb') as pdf:
+    # Create PdfFileReader
+    reader = PyPDF2.PdfReader(pdf)
+    # Get an engine instance for the speech synthesis
+    speak  = pyttsx3.init()
+    # Iterate each page and read the text
+    for page_num in range(len(reader.pages)):
+      page = reader.pages[page_num]
+      text = page.extract_text()
+      if text:
+        speak.say(text)
+        speak.runAndWait()
+
+    # Stop the speech engine
+    speak.stop()
+
+    print('Criação do audiobook finalizada')
+except FileNotFoundError:
+  print('Arquivo não encontrado')
+except Exception as e:
+  print(f'Um erro ocorreu: {e}')
